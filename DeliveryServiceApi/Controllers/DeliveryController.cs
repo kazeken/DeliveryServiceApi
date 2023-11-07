46cee4e8a8d9a76ede52fe974793319c2479952d
kazeken <81683480+kazeken@users.noreply.github.com>
2023-11-07 18:08:07 +0600

    Add project files.

---
+++ DeliveryServiceApi/Controllers/DeliveryController.cs
@@ -0,0 +1,56 @@
+﻿using DeliveryServiceApi.Data;
+using DeliveryServiceApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly ApplicationDbContext _dbContext;
+
+        public DeliveryController(IOrderService orderService, ApplicationDbContext dbContext)
+        {
+            _orderService = orderService;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("check-status")]
+        public IActionResult CheckStatus()
+        {
+            return Ok("Active");
+        }
+
+        [HttpPost("send-order")]
+        public IActionResult SendOrder()
+        {
+            try
+            {
+                if (_orderService.IsFreeCourierAvailable())
+                    return Ok("Order has been sent");
+
+                return NotFound("Available courier not found");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Order rejected");
+            }
+
+        }
+
+        [HttpGet("orders-count")]
+        public IActionResult GetOrdersCount()
+        {
+            int ordersCount = _dbContext.Orders.Count();
+
+            return Ok(ordersCount);
+        }
+       
+    }
+}
---
+++ DeliveryServiceApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DeliveryServiceApi.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
